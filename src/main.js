01ffc705494aee7a1b1ea28ebf1936ded6ceafa3
Root <940150534@qq.com>
2017-05-15 10:37:46 +0800

    V1.0上传
    vue-cli 导入jquery sass bootstrap，接口跨域转接至10，

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import $ from 'jquery'
+import 'bootstrap-loader'
+import '@/assets/css/common.scss'
+import Header from '@/components/header'
+
+Vue.component('htmlHeader', Header)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+})
